d83719a4b7c6e0e91991b90143f82db69ebb28f2
Luke <luke.rooney@me.com>
2016-11-17 00:30:39 -0800

    updated object attribute editor, still only dummy vals

--- src/window/attributeeditor/ObjectAttributes.java
+++ src/window/attributeeditor/ObjectAttributes.java
@@ -4,8 +4,10 @@ import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
@@ -16,50 +18,118 @@ public class ObjectAttributes extends JPanel {
 	private GridBagConstraints c;
 	private JLabel objectPropertiesLabel;
 	private JLabel colorLabel;
+	private JComboBox colorList;
+	private JLabel widthLabel;
+	private JSpinner widthSpinner;
+	private JLabel heightLabel;
+	private JSpinner heightSpinner;
+	private JLabel xPosLabel;
+	private JSpinner xPosSpinner;
+	private JLabel yPosLabel;
+	private JSpinner yPosSpinner;
 	private JLabel currentVelocityLabel;
+	private JLabel angleLabel;
 	private JSpinner velocitySpinner;
+	private JSpinner angleSpinner;
 	
 	public ObjectAttributes() {
 		setLayout(new GridBagLayout());
 		c = new GridBagConstraints();
 		
-		//c.weightx = 0.5;
+		int y = 0;
 		
 		objectPropertiesLabel = new JLabel("Object Properties");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridwidth = 3;
-		c.gridheight = 1;
-		c.gridx = 1;
-		c.gridy = 0;
+		c.gridx = 0;
+		c.gridy = y++;
 		add(objectPropertiesLabel, c);
-		
-		
-		colorLabel = new JLabel("Color: Blue");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridwidth = 2;
+			
+		// color attribute
+		colorLabel = new JLabel("Color: ");
 		c.gridheight = 1;
 		c.gridx = 0;
-		c.gridy = 1;
+		c.gridy = y;
 		add(colorLabel, c);
+		
+		String[] colors = {"Black", "Blue", "Red", "Green", "Purple" };
+		colorList = new JComboBox(colors);
+		colorList.setSelectedIndex(1);
+		c.gridx = 1;
+		c.gridy = y++;
+		add(colorList, c);
 				
-		currentVelocityLabel = new JLabel("Velocity: ");
-		c.fill = GridBagConstraints.NONE;
-		c.gridwidth = 2;
-		c.gridheight = 1;
+		// dimension attributes
+		widthLabel = new JLabel("width: ");
 		c.gridx = 0;
-		c.gridy = 2;
+		c.gridy = y;
+		add(widthLabel, c);
+		
+		widthSpinner = createSpinner(1.0, 0.1, 20, 0.5);
+		c.gridx = 1;
+		c.gridy = y++;
+		add(widthSpinner, c);
+		
+		heightLabel = new JLabel("height: ");
+		c.gridx = 0;
+		c.gridy = y;
+		add(heightLabel, c);
+		
+		heightSpinner = createSpinner(1.0, 0.1, 20, 0.5);
+		c.gridx = 1;
+		c.gridy = y++;
+		add(heightSpinner, c);
+		
+		// position attributes
+		xPosLabel = new JLabel("x position: ");
+		c.gridx = 0;
+		c.gridy = y;
+		add(xPosLabel, c);
+		
+		xPosSpinner = createSpinner(5.0, 0.0, 100.0, 1.0);
+		c.gridx = 1;
+		c.gridy = y++;
+		add(xPosSpinner, c);
+		
+		yPosLabel = new JLabel("y position: ");
+		c.gridx = 0;
+		c.gridy = y;
+		add(yPosLabel, c);
+		
+		yPosSpinner = createSpinner(0, 0.0, 100.0, 1.0);
+		c.gridx = 1;
+		c.gridy = y++;
+		add(yPosSpinner, c);
+		
+		// velocity attribute
+		currentVelocityLabel = new JLabel("Velocity: ");
+		c.gridx = 0;
+		c.gridy = y;
 		add(currentVelocityLabel, c);
 		
 		
-		velocitySpinner = new JSpinner(new SpinnerNumberModel(15.0, -500.0, 500.0, 1.0));
+		velocitySpinner = createSpinner(15.0, -500.0, 500.0, 1.0);
+		c.gridx = 1;
+		c.gridy = y++;
+		add(velocitySpinner, c);
 		
-		Component spinnerEditor = velocitySpinner.getEditor();
+		
+		// angle attribute
+		angleLabel = new JLabel("Angle: ");
+		c.gridx = 0;
+		c.gridy = y;
+		add(angleLabel, c);
+		
+		angleSpinner = createSpinner(60, -179, 180, 1.0);
+		c.gridx = 1;
+		c.gridy = y++;
+		add(angleSpinner, c);
+	}
+	
+	private JSpinner createSpinner(double value, double min, double max, double stepSize) {
+		JSpinner spinner = new JSpinner(new SpinnerNumberModel(value, min, max, stepSize));
+		Component spinnerEditor = spinner.getEditor();
 		JFormattedTextField jftf = ((JSpinner.DefaultEditor) spinnerEditor).getTextField();
 		jftf.setColumns(3);
-		c.fill = GridBagConstraints.NONE;
-		c.gridx = 2;
-		c.gridy = 2;
-		add(velocitySpinner, c);
+		return spinner;
 	}
 	
 }
